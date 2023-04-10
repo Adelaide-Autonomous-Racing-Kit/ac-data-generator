898bf288d1b77e95c84a77f60ef1657767d2c851
James Bockman <james@bockman.dev>
2023-04-10 15:52:24 +0930

    isort

--- src/tracks/tracks.py
+++ src/tracks/tracks.py
@@ -1,5 +1,5 @@
-from src.tracks.monza import MONZA_DATA
 from src.tools.data_generation.tracks.spa import SPA_DATA
+from src.tracks.monza import MONZA_DATA
 
 """
 Register TrackData instances here to make them available during data generation
