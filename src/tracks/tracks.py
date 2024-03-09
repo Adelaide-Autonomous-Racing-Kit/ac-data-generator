42411ba81a0bdce86375a6acd9b112eb98ba77b2
James Bockman <39457784+XDynames@users.noreply.github.com>
2024-03-09 21:36:30 +1030

    Vallelunga (#7)
    * Vallelunga init
    
    * Initial material assignment
    
    * Support for Vallelunga
    
    ---------
    
    Co-authored-by: James Bockman <james@bockman.dev>

--- src/tracks/tracks.py
+++ src/tracks/tracks.py
@@ -1,6 +1,7 @@
 from src.tracks.silverstone import SILVERSTONE_DATA
 from src.tracks.spa import SPA_DATA
 from src.tracks.monza import MONZA_DATA
+from src.tracks.vallelunga import VALLELUNGA_DATA
 
 """
 Register TrackData instances here to make them available during data generation
@@ -10,4 +11,5 @@ TRACK_DATA = {
     "monza": MONZA_DATA,
     "silverstone": SILVERSTONE_DATA,
     "spa": SPA_DATA,
+    "vallelunga": VALLELUNGA_DATA,
 }
