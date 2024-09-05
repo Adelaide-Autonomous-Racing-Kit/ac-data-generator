61f3d0e64f719ebc82e8446653a1e01cee307bca
XDynames <james@bockman.dev>
2024-09-05 16:47:07 +0930

    Updated merge folders script and docker compose, added semantics for mount panorama

--- scripts/merge_folders.py
+++ scripts/merge_folders.py
@@ -1,46 +1,53 @@
+import argparse
 import shutil
 from pathlib import Path
 from concurrent.futures import as_completed, ThreadPoolExecutor
 
 from tqdm import tqdm
 
-root = Path(
-    "/home/james/Desktop/"
-)
-folder_a = root.joinpath("out_1")
-folder_b = root.joinpath("")
-out = root.joinpath("val")
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-1", type=str, help="Path to folder to be merged")
+    parser.add_argument("--input-2", type=str, help="Path to folder to be merged")
+    parser.add_argument("--output", type=str, help="Path to output folfer")
+    return parser.parse_args()
 
 
-def copy_sample_files(input_image: str, i: int):
+def copy_sample_files(output_path: Path, input_image: Path, i: int):
     # Copy image
-    output_image = out.joinpath(f"{i}.jpeg")
+    output_image = output_path.joinpath(f"{i}.jpeg")
     shutil.copyfile(input_image, output_image)
     # Copy class ids
     if Path(f"{str(input_image).split('.')[0]}-trainids.png").is_file():
         input_train_ids = f"{str(input_image).split('.')[0]}-trainids.png"
     else:
         input_train_ids = f"{str(input_image).split('.')[0]}-ids.png"
-    output_train_ids = out.joinpath(f"{i}-ids.png")
+    output_train_ids = output_path.joinpath(f"{i}-ids.png")
     shutil.copyfile(input_train_ids, output_train_ids)
     # Copy visualisation
     if Path(f"{str(input_image).split('.')[0]}-seg_colour.png").is_file():
         input_visualisation = f"{str(input_image).split('.')[0]}-seg_colour.png"
     else:
         input_visualisation = f"{str(input_image).split('.')[0]}-colour.png"
-    output_visualisation = out.joinpath(f"{i}-colour.png")
+    output_visualisation = output_path.joinpath(f"{i}-colour.png")
     shutil.copyfile(input_visualisation, output_visualisation)
 
 
 def main():
+    args = parse_arguments()
+    output_path = Path(args.output)
     promises = []
-    out.mkdir(parents=True, exist_ok=True)
-    input_images = sorted(list(folder_a.glob("*.jpeg")))
-    input_images.extend(sorted(list(folder_b.glob("*.jpeg"))))
+    output_path.mkdir(parents=True, exist_ok=True)
+    input_images = sorted(list(Path(args.input_1).glob("*.jpeg")))
+    if args.input_2:
+        input_images.extend(sorted(list(Path(args.input_2).glob("*.jpeg"))))
     progress_bar = tqdm(total=len(input_images))
     with ThreadPoolExecutor(max_workers=8) as executor:
         for i, input_image in enumerate(input_images):
-            promises.append(executor.submit(copy_sample_files, input_image, i))
+            promises.append(
+                executor.submit(copy_sample_files, output_path, input_image, i)
+            )
         for promise in as_completed(promises):
             progress_bar.update(1)
 
--- src/acdg/tracks/tracks.py
+++ src/acdg/tracks/tracks.py
@@ -4,6 +4,7 @@ from acdg.tracks.silverstone import SILVERSTONE_DATA
 from acdg.tracks.spa import SPA_DATA
 from acdg.tracks.vallelunga import VALLELUNGA_DATA
 from acdg.tracks.yas_marina import YAS_MARINA_DATA
+from acdg.tracks.mt_panorama import MT_PANORAMA_DATA
 
 """
 Register TrackData instances here to make them available during data generation
@@ -16,4 +17,5 @@ TRACK_DATA = {
     "vallelunga": VALLELUNGA_DATA,
     "nordschleife": NORDSCHLEIFE_DATA,
     "yas_marina": YAS_MARINA_DATA,
+    "mount_panorama": MT_PANORAMA_DATA,
 }
